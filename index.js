e9f39bb9710eed0a6b1d7d61bacd51d4b3e9cb27
m-damas <elvenmaiden438@yahoo.com>
2023-10-30 13:55:29 -0500

    trying to add forecast to js

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ let day = days[now.getDay()];
 h3.innerHTML = `${day} ${hours}:${minutes}`;
 
 //
-function displayForecast() {
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -54,12 +55,19 @@ function displayForecast() {
      </div>
   </div>
 </div>
-</div>
+
 `;
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "6f5688286a7b5cd7f89135c7b3cafe9f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function searchForm(search) {
@@ -216,8 +224,6 @@ currentButton.addEventListener("click", getCurrentPosition);
 //
 
 let celsiusTemperature = null;
-
-displayForecast();
 
 let displayFahrenheit = document.querySelector(`#fahrenheit`);
 displayFahrenheit.addEventListener("click", displayFahrenheitTemperature);
